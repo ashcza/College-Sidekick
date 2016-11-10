b1ba96acef06ce1a509bd535105481b68a8df3f2
ashcza <ashcon.zand@gmail.com>
2016-11-09 23:07:47 -0800

    Site up

---
+++ frontend/index.jsx
@@ -0,0 +1,9 @@
+//React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  let message = <div>It's working</div>;
+  const root = document.getElementById('root');
+  ReactDOM.render(message, root);
+});
